30089764a053cc10f9eff998fe410f184a129672
henrylai <e0032134@u.nus.edu>
2022-05-17 20:11:56 +0800

    add drop down for product search

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -9,6 +9,16 @@ import { Row, Col, Card, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import UsersContext from '../../contexts/UsersContext';
 // import { BASE_URL } from '../../constant/Constants';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
+
 
 const BASE_URL = "https://3000-henryheyhey-espressoexp-1blfs1n110r.ws-us45.gitpod.io/"
 
@@ -19,20 +29,48 @@ export default function Products() {
     const [textSearch, setTextSearch] = useState({
         productKeyword: ""
     });
+
+    const [formSearch, setForm] = useState({
+        product_text: "",
+        min_price: "",
+        max_price: "",
+        roast_type: ""
+    })
+
+    const [roastType, setRoastType] = useState([]);
+    const [certType, setCertType] = useState();
+    const [originType, setOriginType] = useState();
+
     let context = useContext(UsersContext);
     //componment didmout
     useEffect(() => {
         const fetchProduct = async () => {
+            console.log("roats type hahaha")
             let response = await axios.get(BASE_URL + 'api/products');
+            let roastRes = await axios.get(BASE_URL + 'api/products/get/all/roast/type');
+            let certRes = await axios.get(BASE_URL + 'api/products/get/cert/type');
+            let originRes = await axios.get(BASE_URL + 'api/products/get/country/origin');
+
+            setRoastType(roastRes.data);
+            setCertType(certRes.data);
+            setOriginType(originRes.data);
             setProduct(response.data);
             console.log(response.data);
-            console.log(typeof(response.data));
         }
         fetchProduct()
     }, [])
 
+    useEffect(() => {
+        console.log(roastType)
+    }, [roastType])
+
     const onUpdateFormField = (e) => setTextSearch({
         ...textSearch,
+        [e.target.name]: e.target.value
+    })
+
+    const onUpdateSearchFormField = (e) => setForm({
+        ...formSearch,
         [e.target.name]: e.target.value
     })
 
@@ -52,15 +90,10 @@ export default function Products() {
                 'product_id': e.target.value
             }
             let response = await axios.post(BASE_URL + 'api/shoppingCart/additem', requestBodyData, { headers });
-
-            console.log("check add cart result whether is true");
-            console.log(response.data);
         } else {
             //need to prom for get new access token or ask user to sign in
-            console.log("get new access token")
             let result = await context.getRefreshToken();
             if (result) {
-                console.log("call add to cart")
                 addToCart(e);
             }
 
@@ -77,23 +110,178 @@ export default function Products() {
         setProduct(response.data);
     }
 
+    const searchAllField = async () => {
+
+        //to get the roast type index to query the coffee bean roast type id
+        let roastTypeIdentifier = null;
+        for(let e of roastType){
+            if(e[1] === formSearch.roast_type){
+                roastTypeIdentifier = e[0]
+            }
+        }
+        ////
+
+        const reqbody = {
+            'min_price': formSearch.min_price,
+            'max_price': formSearch.max_price,
+            'roast_type': roastTypeIdentifier
+        }
+        let response = await axios.post(BASE_URL + 'api/products/filter/by', reqbody);
+        console.log(response.data);
+        setProduct(response.data)
+    }
+
+    // const renderRadioOption = () => {
+    //     let options = [];
+    //     let temp = [];
+    //     //get the data 
+    //     if (roastType) {
+    //         temp = roastType
+    //         console.log("Print temp");
+    //         console.log(temp);
+    //     }
+    //     if (temp) {
+    //         for (let o of temp) {
+    //             options.push(
+    //                 <FormControlLabel
+    //                     key={o.id}  
+    //                     // value={o.value}
+    //                     control={<Radio />}
+    //                     label={o.name}
+    //                     name='roastType'
+    //                     // onChange={this.updateFormField}
+    //                     // checked={this.state.category.includes(o.value)}
+    //                     style={{ minWidth: 125 }} />
+    //             )
+    //             console.log(o)
+    //         }
+    //     }
+
+    //     return options;
+    // }
+
+    // renderCheckboxOption() {
+    //     let temp = [];
+    //     if (this.state.mediumOptions.medium) {
+    //         temp = this.state.mediumOptions.medium
+    //     }
+    //     if (temp) {
+    //         return temp.map(e => {
+    //             return <React.Fragment key={e.value}>
+    //                 <FormControlLabel
+    //                     control={
+    //                         <Checkbox
+    //                             name="medium"
+    //                             value={e.value}
+    //                             onChange={this.updateCheckboxes}
+    //                             checked={this.state.medium.includes(e.value)}
+    //                             sx={{ pr: 1 }}
+    //                         />
+    //                     } label={e.name}
+    //                 />
+    //             </React.Fragment>
+    //         })
+    //     }
+    //     return temp
+    // }
+
     return (
         <React.Fragment>
             <Typography className="lead fs-2" sx={{ ml: 2 }}>Coffee Bean Products</Typography>
-            <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 'auto', m:2 }}>
+            <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 'auto', m: 2 }}>
                 <InputBase
                     sx={{ ml: 1, flex: 1 }}
                     placeholder="Search"
                     inputProps={{ 'aria-label': 'search google maps' }}
-                    name="productKeyword" 
+                    name="productKeyword"
                     value={textSearch.productKeyword}
                     onChange={onUpdateFormField}
                 />
-                <IconButton type="submit" sx={{ p: '10px' }} 
-                            aria-label="search"  
-                            onClick={searchBytext}>
+                <IconButton type="submit" sx={{ p: '10px' }}
+                    aria-label="search"
+                    onClick={searchBytext}>
                     <SearchIcon />
                 </IconButton>
+
+                {/* other search feature */}
+
+                {/* <Paper component="form" sx={{ p: '2px 4px', width: "100%", mt: 3 }}>
+                    <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+                        <FormLabel id="checkbox-group-label">Certificate</FormLabel>
+                        <FormGroup
+                            sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-evenly" }}>
+                            {renderCheckboxOption()}
+                        </FormGroup>
+                    </FormControl>
+                </Paper>
+
+                <Paper component="form" sx={{ p: '2px 4px', width: "100%", mt: 3 }}>
+                    <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+                        <FormLabel id="checkbox-group-label">Origin</FormLabel>
+                        <FormGroup
+                            sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-evenly" }}>
+                            {renderCheckboxOption()}
+                        </FormGroup>
+                    </FormControl>
+                </Paper> */}
+
+            </Paper>
+
+            <Paper sx={{ p: '2px 4px', width: "auto", minheight: "200px", m: 3 }} elevation={3} >
+                <div>
+                    <label>Text Search</label>
+                    <input type="text"
+                        placeholder="Product text search"
+                        name='product_text'
+                        onChange={onUpdateSearchFormField}
+                        value={formSearch.product_text}
+                    />
+                </div>
+                <div>
+                    <label>Example select</label>
+                    <select name="roast_type"
+                            onChange={onUpdateSearchFormField}
+                            value={formSearch.roast_type}>
+                        <option>---------</option>
+                        {roastType ? roastType.map((element, i) => {
+                            return <option key={element[1]}
+                                    value={element[1]}>{element[1]}</option>
+                        }):  <option>---------</option>}
+                        
+                    </select>
+                </div>
+                <div>
+                    <label>Min price</label>
+                    <input type="text"
+                        placeholder="Min price"
+                        name='min_price'
+                        onChange={onUpdateSearchFormField}
+                        value={formSearch.min_price}
+                    />
+                </div>
+                <div>
+                    <label>Max price</label>
+                    <input type="text"
+                        placeholder="Max price"
+                        name='max_price'
+                        onChange={onUpdateSearchFormField}
+                        value={formSearch.max_price} />
+                </div>
+                <button class="btn btn-primary" onClick={searchAllField}>Search</button>
+
+            </Paper>
+
+            <Paper component="form" sx={{ p: '2px 4px', width: "auto", m: 3 }} elevation={3} >
+                <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+                    <FormLabel id="category-group-label">Roast Type</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        name="radio-buttons-group"
+                        sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-evenly" }}
+                    >
+                        {/* {renderRadioOption} */}
+                    </RadioGroup>
+                </FormControl>
             </Paper>
             {/* add in accordino */}
 
@@ -106,6 +294,12 @@ export default function Products() {
                                 <Card.Body key={p.product_name}>
                                     <Card.Text className="lead fs-5" >
                                         {p.product_name} ({p.description})
+                                        <Card.Text sx={{ m: 1 }}>
+                                            ({p.price}) 
+                                        </Card.Text>
+                                        <Card.Text sx={{ m: 1 }}>
+                                            ({p.roastType.name}) 
+                                        </Card.Text>
                                     </Card.Text>
                                 </Card.Body>
                             </Link>
@@ -116,7 +310,7 @@ export default function Products() {
                             >Add to Cart</Button>
                         </Card>
                     </Col>
-                )): <h1>Loading</h1>}
+                )) : <h1>Loading</h1>}
             </Row>
 
         </React.Fragment>
